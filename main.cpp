7c5e912b26bd4d6ca6914f5b1771aef669ea6828
An.Phan <an.phan@ascenx.com>
2017-05-22 09:27:23 +0700

    added andebug.h

--- main.cpp
+++ main.cpp
@@ -1,18 +1,22 @@
 #include <QCoreApplication>
 #include <QTime>
 #include <QProcess>
+#include <QFile>
 #include <QDir>
-#include <QDebug>
+#include "andebug.h"
 
 #ifdef Q_OS_WIN
 #include <windows.h>
-#define TildeDirectory D:/home/pi
+#define _TildeDirectory "D:/home/pi"
 #else
 #include <time.h>
-#define TildeDirectory /home/pi
+#define _TildeDirectory "/home/pi"
 #endif
 
-void GoSleep(quint64 ms)
+#define _DefaultPollingRate 1
+#define _DefaultCheckPoint QTime::fromString("14:30:00","hh:mm:ss")
+
+void GoSleep(int ms)
 {
 #ifdef Q_OS_WIN
     Sleep(ms);
@@ -25,20 +29,45 @@ void GoSleep(quint64 ms)
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    QTime qtiming;
     QProcess * proc = new QProcess();
     quint8 count0 = 0;
-    qDebug() << QDir().mkpath("D:/abc/ngoc/okay");
+    quint8 PollingRate = 16;//_DefaultPollingRate;
+    QTime CheckPoint = QTime::fromString("16:36:00","hh:mm:ss");//_DefaultCheckPoint;
     while (true)
     {
-        qtiming.start();
-
-        while ((++count0)<=7)
+        //Reinitializing Variable
+        count0 = 0;
+        //<Start Timing Here If Needed>
+        QFile::remove(_TildeDirectory "/AutoUpdatePiSG/AutoUpdatePiSG.conf");
+        do
         {
+            if (++count0>7)
+            {
+                break;
+            }
+#ifdef Q_OS_WIN
+            anDebug("Please MANUALLY Download AutoUpdatePiSG.conf And Place It Into " _TildeDirectory "/AutoUpdatePiSG/ !!!");
+#else
+            proc->execute("wget -P " _TildeDirectory "\"http://www.orimi.com/pdf-test.pdf\"");
+#endif
+        } while (!QFile::exists(_TildeDirectory "/AutoUpdatePiSG/AutoUpdatePiSG.conf"));
+        if (count0<=7)
+        {
+            count0 = 0;
 
         }
-        if (count0 )
-        //--Take A Break--
+        //<Stop Timing Here If Needed>
+        int BreakInterval = 86400000/PollingRate;
+        int TimePoint = CheckPoint.msecsSinceStartOfDay() - BreakInterval;
+        int SleepTime;
+        do
+        {
+            TimePoint += BreakInterval;
+            SleepTime = TimePoint -QTime::currentTime().msecsSinceStartOfDay();
+        } while (SleepTime < 0);
+        anDebug("Sleep Until" << QTime::fromMSecsSinceStartOfDay(TimePoint).toString("hh:mm:ss"));
+        anqDebug(_VarView(SleepTime) + " milisecond");
+        GoSleep(SleepTime);
     }
     return a.exec();
 }
